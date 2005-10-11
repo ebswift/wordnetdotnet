a7b2019e3ca02d671bee90e3c307f0309fbf9852
ebswift <ebswift@02d3d6db-b522-0410-a969-658e03a0e743>
2005-10-11 20:21:59 +0000

    Implemented SemCor into the library and placed a working sample in the TreeView sample.

--- trunk/Library/cs/SemCor.cs
+++ trunk/Library/cs/SemCor.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.IO;
 
 namespace Wnlib
@@ -12,12 +13,16 @@ namespace Wnlib
 
 		public SemCor(Lexeme lex, int hereiam)
 		{
+			// left-pad the integer with 0's into a string
 			string key = hereiam.ToString("d8") + " " + lex.wnsns;
 
             System.IO.StreamReader indexFile = new System.IO.StreamReader(Wnlib.WNDB.path + @"\index.sense");
 
+			// locate our word and key via a binary search
 			string semline = Wnlib.WNDB.binSearchSemCor(key, lex.word, indexFile);
 
+			string [] lexinfo = semline.Split(' ');
+			semcor = Convert.ToInt16(lexinfo[lexinfo.GetUpperBound(0)]);
 		}
 	}
 }
