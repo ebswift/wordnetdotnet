8914a89d105b090b6dafa61648b1139c9ddfe2c3
ebswift <ebswift@02d3d6db-b522-0410-a969-658e03a0e743>
2006-02-10 03:53:16 +0000

    Handled the right-click action node identification in the TreeControl, minimising the work needed on that event in the control's parent, whilst maintaining customisation of the action handling at the parent.  Tidied up the TreeView vb sample comments and the TreeControl comments.

--- Samples/vb/Controls/WordNetControls/TreeControl.vb
+++ Samples/vb/Controls/WordNetControls/TreeControl.vb
@@ -26,13 +26,17 @@
 ' User: simpsont
 ' Date: 10/02/2006
 ' Time: 10:25 AM
-' 
-' TreeControl
-' Standalone TreeView control for WordNet.
-' See the TreeView sample under the samples folder for an example of use
+
+'TODO: icons need to be created/obtained and appropriately assigned to the various results types.
+
+''' <summary>
+''' Standalone TreeView control for WordNet.  
+''' See the TreeView sample under the samples folder for an example of use.
+''' </summary>
 Public Partial Class TreeControl
 	Public Event AfterSelect(sender As Object, e As System.Windows.Forms.TreeViewEventArgs)
 	Public Shadows Event MouseDown(sender As Object, e As System.Windows.Forms.MouseEventArgs)
+	Public Event TreeRightClick(sender As Object, e As System.Windows.Forms.MouseEventArgs, tn As System.Windows.Forms.TreeNode)
 	
 	Public Sub New()
 		' The Me.InitializeComponent call is required for Windows Forms designer support.
@@ -43,107 +47,125 @@ Public Partial Class TreeControl
 		'
 	End Sub
 	
-        Public Sub fillTreeRoot(ByVal sch As Wnlib.Search, ByVal opt As Wnlib.Opt, Optional ByVal tmppos As String = "")
-            ' do the treeview
-            Dim ss As Wnlib.SynSet
-            Dim parentnode As TreeNode
-            Dim posnode As TreeNode = Nothing ' part of speech node used for overview search
+	''' <summary>
+	''' Fill the top level of the tree.
+	''' </summary>
+	''' <param name="sch"></param>
+	''' <param name="opt">Currently a redundant parameter, but since it holds the search type, it can remain in case search type needs to be known.</param>
+	''' <param name="tmppos"></param>
+    Public Sub fillTreeRoot(ByVal sch As Wnlib.Search, ByVal opt As Wnlib.Opt, Optional ByVal tmppos As String = "")
+        ' do the treeview
+        Dim ss As Wnlib.SynSet
+        Dim parentnode As TreeNode
+        Dim posnode As TreeNode = Nothing ' part of speech node used for overview search
 
-            ' a part of speech has been given as a parameter
-            ' so create a new top level node
-            If tmppos <> "" Then
-                posnode = New TreeNode(tmppos)
-                TreeView1.Nodes.Add(posnode)
+        ' a part of speech has been given as a parameter
+        ' so create a new top level node
+        If tmppos <> "" Then
+            posnode = New TreeNode(tmppos)
+            TreeView1.Nodes.Add(posnode)
+        End If
+
+        ' iterate the returned senses
+        For Each ss In sch.senses
+            parentnode = newTreeNode(ss)
+
+            ' if a part of speech node has been created
+            ' then it becomes the top level
+            If Not posnode Is Nothing Then
+                posnode.Nodes.Add(parentnode)
+            Else
+                TreeView1.Nodes.Add(parentnode)
             End If
 
-            ' iterate the returned senses
-            For Each ss In sch.senses
-                parentnode = newTreeNode(ss)
+            ' do child senses
+            If Not ss.senses Is Nothing Then
+                fillTreeChild(ss.senses, parentnode)
+            End If
 
-                ' if a part of speech node has been created
-                ' then it becomes the top level
-                If Not posnode Is Nothing Then
-                    posnode.Nodes.Add(parentnode)
-                Else
-                    TreeView1.Nodes.Add(parentnode)
-                End If
+            ' fill in sense frames
+            Dim fr As Wnlib.SynSetFrame
+            Dim frnode As TreeNode
+            If ss.frames.Count > 0 Then
+                For Each fr In ss.frames
+                    frnode = New TreeNode(fr.fr.str)
+                    frnode.ImageIndex = 23 'TODO: change this number
+                    parentnode.Nodes.Add(frnode)
+                Next
+            End If
 
-                ' do child senses
-                If Not ss.senses Is Nothing Then
-                    fillTreeChild(ss.senses, parentnode)
-                End If
+            'skip:
+        Next ss
+    End Sub
 
-                ' fill in sense frames
-                Dim fr As Wnlib.SynSetFrame
-                Dim frnode As TreeNode
-                If ss.frames.Count > 0 Then
-                    For Each fr In ss.frames
-                        frnode = New TreeNode(fr.fr.str)
-                        frnode.ImageIndex = 23 'TODO: change this number
-                        parentnode.Nodes.Add(frnode)
-                    Next
-                End If
+	''' <summary>
+	''' Fill all the child nodes.  This is done to allow recursive child calls.
+	''' </summary>
+	''' <param name="ssarray">Search Sense Array</param>
+	''' <param name="parentnode">Parent which the child is going to attach</param>
+    Private Sub fillTreeChild(ByVal ssarray As Wnlib.SynSetList, ByVal parentnode As TreeNode)
+        Dim childnode As TreeNode
+        Dim ss As Wnlib.SynSet
 
-                'skip:
-            Next ss
-        End Sub
-
-        ' separate sub to fill all the children -
-        ' this is done to allow recursive child calls
-        Private Sub fillTreeChild(ByVal ssarray As Wnlib.SynSetList, ByVal parentnode As TreeNode)
-            Dim childnode As TreeNode
-            Dim ss As Wnlib.SynSet
-
-            For Each ss In ssarray
-                ' define a new child node
-                childnode = newTreeNode(ss)
-
-                parentnode.Nodes.Add(childnode)
-
-                ' increase the depth of the tree via recursion
-                If Not ss.senses Is Nothing Then
-                    fillTreeChild(ss.senses, childnode)
-                End If
-            Next ss
-        End Sub
-
-        Private Function newTreeNode(ByVal ss As Wnlib.SynSet) As TreeNode
-            Dim word As Wnlib.Lexeme
-            Dim words As String
-            Dim childnode As TreeNode
-
-            words = ""
-
-            ' these are the returned lexemes
-            For Each word In ss.words
-                If words <> "" Then
-                    words += ", "
-                End If
-
-                words += Replace(word.word, "_", " ")
-
-                ' append the sense number when the sense is not 1
-                If word.wnsns <> 1 Then
-                    words += "(" & word.wnsns & ")"
-                End If
-            Next word
-
+        For Each ss In ssarray
             ' define a new child node
-            childnode = New TreeNode
-            childnode.Text = words
-            'childnode.Tag = ss.defn
+            childnode = newTreeNode(ss)
 
-            ' assign an icon according to the ident number of the ptr
-            ' (see static void classinit() in util.cs)
-            If Not ss.thisptr Is Nothing Then
-                childnode.ImageIndex = ss.thisptr.ptp.ident
+            parentnode.Nodes.Add(childnode)
+
+            ' increase the depth of the tree via recursion
+            If Not ss.senses Is Nothing Then
+                fillTreeChild(ss.senses, childnode)
+            End If
+        Next ss
+    End Sub
+
+	''' <summary>
+	''' Standardises the addition of a new TreeNode to the TreeView.  Lexemes are iterated and comma-separated
+	''' for display purposes.  An icon is attached to the node according to the type of the sense.
+	''' </summary>
+	''' <param name="ss"></param>
+	''' <returns></returns>
+    Private Function newTreeNode(ByVal ss As Wnlib.SynSet) As TreeNode
+        Dim word As Wnlib.Lexeme
+        Dim words As String
+        Dim childnode As TreeNode
+
+        words = ""
+
+		' Build the words for display in the node.
+		' Words are the lexemes in a search result.
+        For Each word In ss.words
+            If words <> "" Then
+                words += ", "
             End If
 
-            childnode.Tag = ss
+            words += Replace(word.word, "_", " ")
 
-            Return childnode
-        End Function
+            ' append the sense number when the sense is not 1
+            If word.wnsns <> 1 Then
+                words += "(" & word.wnsns & ")"
+            End If
+        Next word
+
+        ' define a new child node
+        childnode = New TreeNode
+        childnode.Text = words
+
+        ' assign an icon according to the ident number of the ptr
+        ' (see static void classinit() in util.cs)
+        If Not ss.thisptr Is Nothing Then
+            childnode.ImageIndex = ss.thisptr.ptp.ident
+        End If
+
+        childnode.Tag = ss
+
+        Return childnode
+    End Function
 	
+	''' <summary>
+	''' Gets or sets the currently selected node in the tree.
+	''' </summary>
 	Public Property SelectedNode As System.Windows.Forms.TreeNode
 		Get
 			Return TreeView1.SelectedNode
@@ -153,28 +175,63 @@ Public Partial Class TreeControl
 		End Set
 	End Property
 
+	''' <summary>
+	''' Gets the TreeNode at position x, y.
+	''' </summary>
 	Public ReadOnly Property GetNodeAt(ByVal x As Integer, ByVal y As Integer) As System.Windows.Forms.TreeNode
 		Get
 			Return TreeView1.GetNodeAt(x, y)
 		End Get
 	End Property
 
+	''' <summary>
+	''' Passes along the AfterSelect event from the TreeView.
+	''' </summary>
+	''' <param name="sender"></param>
+	''' <param name="e"></param>
 	Sub TreeView1AfterSelect(sender As Object, e As System.Windows.Forms.TreeViewEventArgs)
 		RaiseEvent AfterSelect(sender, e)
 	End Sub
 	
+	''' <summary>
+	''' If the action was a right-click find the node that was at the right-click position and pass it along
+	''' with the custom TreeRightClick event.
+	''' </summary>
+	''' <param name="sender"></param>
+	''' <param name="e"></param>
 	Sub TreeView1MouseDown(sender As Object, e As System.Windows.Forms.MouseEventArgs)
-		RaiseEvent MouseDown(sender, e)
+        If e.Button = MouseButtons.Right Then
+        	' get the treenode at the mouse location
+            Dim t As TreeNode = TreeView1.GetNodeAt(e.X, e.Y)
+
+        	' only proceed if there was a valid clicked node
+        	If t Is Nothing Then
+        		Exit Sub
+        	End If
+
+            TreeView1.SelectedNode = t
+
+			RaiseEvent TreeRightClick(sender, e, t)
+		End If
 	End Sub
 	
+	''' <summary>
+	''' Clears the TreeView nodes.
+	''' </summary>
 	Public Sub Clear()
 		TreeView1.Nodes.Clear()
 	End Sub
 	
+	''' <summary>
+	''' Passes through the BeginUpdate method to the TreeView.
+	''' </summary>
 	Public Sub BeginUpdate()
 		TreeView1.BeginUpdate()
 	End Sub
 
+	''' <summary>
+	''' Passes through the EndUpdate method to the TreeView
+	''' </summary>
 	Public Sub EndUpdate()
 		TreeView1.EndUpdate()
 	End Sub
