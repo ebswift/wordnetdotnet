2f1c5ecf3882fb4b5f26c7452e3787062a58d0dd
ebswift <ebswift@02d3d6db-b522-0410-a969-658e03a0e743>
2009-03-05 00:14:29 +0000

    Bug fix reported by Scott Zhang.  Project upgraded to VS2008.  Report follows:
    When user input a keywords eg. "designer", we could search for "design" too.
        I tried native C version of wordnet. And I can see when I input "designer", by specify the point_type = "NOMINALIZATIONS", we should get words "RELATED TO". So it is working then I could get "design" from input "designer". 
       But the problem is when I use your version (I got lastest version from svn yesterday.) I can't see "Related to" in the search.buf. Others seems ok. I viewed your code. And get confirmed you have wrote the code for "related to". But the code never go that that area. 
    
       This morning, I was carefully comparing your code with the original wordnet source code. And need code to catch why it didn't work.  At last, I found the problem is, when you parse "+", which should be "NOMINALIZATIONS", value (20). 
    
    check http://wordnet.princeton.edu/man/wnsearch.3WN
    DERIVATION 	20 	+ 	Derivationally related form 
    FRAMES 	25 	n/a 	Verb example sentences and generic frames 
    
    
    But in your code, you give 25 the "+" too. which should be "".
    After change "new PointerType("+", "FRAMES", "Sample Sentences"); // 25" to "new PointerType("", "FRAMES", "Sample Sentences"); // 25".
    The problem solved.

--- trunk/Library/cs/util.cs
+++ trunk/Library/cs/util.cs
@@ -249,51 +249,51 @@ namespace Wnlib
 
 		public static string[] lexfiles = 
 		{
-			"adj.all",			/* 0 */
-			"adj.pert",			/* 1 */
-			"adv.all",			/* 2 */
-			"noun.Tops",		/* 3 */
-			"noun.act",			/* 4 */
-			"noun.animal",		/* 5 */
+			"adj.all",			    /* 0 */
+			"adj.pert",			    /* 1 */
+			"adv.all",			    /* 2 */
+			"noun.Tops",		    /* 3 */
+			"noun.act",			    /* 4 */
+			"noun.animal",		    /* 5 */
 			"noun.artifact",		/* 6 */
 			"noun.attribute",		/* 7 */
-			"noun.body",		/* 8 */
+			"noun.body",		    /* 8 */
 			"noun.cognition",		/* 9 */
-			"noun.communication",	/* 10 */
-			"noun.event",		/* 11 */
-			"noun.feeling",		/* 12 */
-			"noun.food",		/* 13 */
-			"noun.group",		/* 14 */
+			"noun.communication",   /* 10 */
+			"noun.event",		    /* 11 */
+			"noun.feeling",		    /* 12 */
+			"noun.food",		    /* 13 */
+			"noun.group",		    /* 14 */
 			"noun.location",		/* 15 */
-			"noun.motive",		/* 16 */
-			"noun.object",		/* 17 */
-			"noun.person",		/* 18 */
+			"noun.motive",		    /* 16 */
+			"noun.object",		    /* 17 */
+			"noun.person",		    /* 18 */
 			"noun.phenomenon",		/* 19 */
-			"noun.plant",		/* 20 */
+			"noun.plant",		    /* 20 */
 			"noun.possession",		/* 21 */
-			"noun.process",		/* 22 */
+			"noun.process",		    /* 22 */
 			"noun.quantity",		/* 23 */
 			"noun.relation",		/* 24 */
-			"noun.shape",		/* 25 */
-			"noun.state",		/* 26 */
+			"noun.shape",		    /* 25 */
+			"noun.state",		    /* 26 */
 			"noun.substance",		/* 27 */
-			"noun.time",		/* 28 */
-			"verb.body",		/* 29 */
-			"verb.change",		/* 30 */
+			"noun.time",		    /* 28 */
+			"verb.body",		    /* 29 */
+			"verb.change",		    /* 30 */
 			"verb.cognition",		/* 31 */
 			"verb.communication",	/* 32 */
 			"verb.competition",		/* 33 */
 			"verb.consumption",		/* 34 */
-			"verb.contact",		/* 35 */
+			"verb.contact",		    /* 35 */
 			"verb.creation",		/* 36 */
-			"verb.emotion",		/* 37 */
-			"verb.motion",		/* 38 */
+			"verb.emotion",		    /* 37 */
+			"verb.motion",		    /* 38 */
 			"verb.perception",		/* 39 */
 			"verb.possession",		/* 40 */
-			"verb.social",		/* 41 */
-			"verb.stative",		/* 42 */
-			"verb.weather",		/* 43 */
-			"adj.ppl",			/* 44 */
+			"verb.social",		    /* 41 */
+			"verb.stative",		    /* 42 */
+			"verb.weather",		    /* 43 */
+			"adj.ppl",			    /* 44 */
 		};
 
 		internal static string ExcFile(PartOfSpeech n)
@@ -723,7 +723,7 @@ namespace Wnlib
 			/* Additional searches, but not pointers.  */
 			new PointerType("", "SYNS", "Synonyms"); // 23
 			new PointerType("", "FREQ", "Frequency"); // 24
-			new PointerType("+", "FRAMES", "Sample Sentences"); // 25
+			new PointerType("", "FRAMES", "Sample Sentences"); // 25 // TDMS 05/03/09 - fix suggested by Scott Zhang - reported 09/01/2009
 			new PointerType("", "COORDS", "Coordinate Terms"); // 26
 			new PointerType("", "RELATIVES", "Relatives"); // 27
 			new PointerType("", "HMERONYM", "Meronyms"); // 28
