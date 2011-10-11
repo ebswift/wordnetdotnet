0a958b1d84d78a3186ff8316918f6ad880e2dba6
ebswift <ebswift@02d3d6db-b522-0410-a969-658e03a0e743>
2011-10-11 22:11:45 +0000

    * conversion of vbnet controls to c# * conversion of vbnet colour formatted sample to c#

---
+++ trunk/Samples/cs/Colour Formatted Sample/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+// Review the values of the assembly attributes
+
+[assembly: AssemblyTitle("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: CLSCompliant(true)]
+//The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("FEF7397B-9248-4557-BDBB-08A913D431A3")]
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.0.*")]
